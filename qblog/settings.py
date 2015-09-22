24d24ae27c72913c06ae7c5dcc9c6ed93bbd21da
realhu1989 <huyunyan@live.cn>
2015-09-22 22:39:16 +0800

    fix syntax error

--- qblog/settings.py
+++ qblog/settings.py
@@ -59,12 +59,12 @@ WSGI_APPLICATION = 'qblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
