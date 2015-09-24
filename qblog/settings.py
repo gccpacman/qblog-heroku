de6a62fb3583ac0501c57e18ee9a7a1cfceda20f
realhu1989 <huyunyan@live.cn>
2015-09-24 22:10:01 +0800

    test db

--- qblog/settings.py
+++ qblog/settings.py
@@ -59,17 +59,17 @@ WSGI_APPLICATION = 'qblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# import dj_database_url
 # DATABASES = {
-#     'default':  dj_database_url.config()
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+import dj_database_url
+DATABASES = {
+    'default':  dj_database_url.config()
+}
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
